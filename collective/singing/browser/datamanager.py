256baeef3d68c9d0b2ed7e94d96350da1a8ed8dd
wimbou <wimbou@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-03-28 09:57:25 +0000

    adding datamapper

---
+++ collective/singing/browser/datamanager.py
@@ -0,0 +1,37 @@
+import zope.component
+import zope.schema
+import zope.interface
+
+import z3c.form.datamanager
+
+class SetToDictField(z3c.form.datamanager.AttributeField):
+    """Puts a Set into a Dict field."""
+    zope.component.adapts(
+        zope.interface.Interface, zope.schema.interfaces.ISet)
+
+    key_attr = 'name'
+        
+    def get(self):
+        """See z3c.form.interfaces.IDataManager"""
+        # get the right adapter or context
+        context = self.context
+        if self.field.interface is not None:
+            context = self.field.interface(context)
+        return getattr(context, self.field.__name__).values()
+
+    def set(self, value):
+        """See z3c.form.interfaces.IDataManager"""
+        if self.field.readonly:
+            raise TypeError("Can't set values on read-only fields "
+                            "(name=%s, class=%s.%s)"
+                            % (self.field.__name__,
+                               self.context.__class__.__module__,
+                               self.context.__class__.__name__))
+        # get the right adapter or context
+        context = self.context
+        if self.field.interface is not None:
+            context = self.field.interface(context)
+        dict = {}
+        for item in value:
+            dict[getattr(item, self.key_attr)] = item          
+        setattr(context, self.field.__name__, dict)
