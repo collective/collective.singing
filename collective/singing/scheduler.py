e7278a01a5c10fa4d5bab97be2ec79c9556ac17d
nouri <nouri@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-12 14:29:09 +0000

    Split up assemble_messages so that we now have a render_message method that we can use to send a message to exactly one subscriber.

--- collective/singing/scheduler.py
+++ collective/singing/scheduler.py
@@ -24,56 +24,56 @@ def getIFormatAdapter(obj, format):
     else:
         return formatter
 
-def assemble_messages(channel, items=(), use_collector=True):
+def render_message(channel, sub, items, use_collector):
     collector = channel.collector
     composers = channel.composers
 
-    queued_messages = 0
+    subscription_metadata = sub.metadata
+    if subscription_metadata.get('pending'):
+        return None
 
+    # Collect items for subscription
+    if collector is not None and use_collector:
+        collector_items, cue = collector.get_items(
+            subscription_metadata.get('cue'), sub)
+        subscription_metadata['cue'] = cue
+
+        # If there was a collector but no items, we'll skip
+        # this subscription:
+        if not items and len(collector_items) == 0:
+            return None
+    else:
+        # If there's no collector, we go on with an empty
+        # items list:
+        collector_items = ()
+
+    final_items = tuple(items) + tuple(collector_items)
+
+    # First format all items...
+    format = subscription_metadata['format']
+    final_items = [getIFormatAdapter(item, format)() for item in final_items]
+    transforms = component.getAllUtilitiesRegisteredFor(
+        interfaces.ITransform)
+
+    # ... then transform them ...
+    transformed_items = []
+    for item in final_items:
+        for transform in transforms:
+            item = transform(item, sub)
+        transformed_items.append(item)
+
+    # ... and finally render using the right composer. Note
+    # that the message is already queued when we render it.
+    composer = composers[format]
+    return composer.render(sub, transformed_items)
+
+def assemble_messages(channel, items=(), use_collector=True):
+    queued_messages = 0
     for secret, subscriptions in channel.subscriptions.items():
         for sub in subscriptions:
-            subscription_metadata = interfaces.ISubscriptionMetadata(sub)
-
-            if subscription_metadata.get('pending'):
-                continue
-
-            # Collect items for subscription
-            if collector is not None and use_collector:
-                collector_items, cue = collector.get_items(
-                    subscription_metadata.get('cue'), sub)
-                subscription_metadata['cue'] = cue
-
-                # If there was a collector but no items, we'll skip
-                # this subscription:
-                if not items and len(collector_items) == 0:
-                    continue
-            else:
-                # If there's no collector, we go on with an empty
-                # items list:
-                collector_items = ()
-
-            final_items = tuple(items) + tuple(collector_items)
-
-            # First format all items...
-            format = subscription_metadata['format']
-            final_items = [getIFormatAdapter(item, format)() for item in final_items]
-            transforms = component.getAllUtilitiesRegisteredFor(
-                interfaces.ITransform)
-
-            # ... then transform them ...
-            transformed_items = []
-            for item in final_items:
-                for transform in transforms:
-                    item = transform(item, sub)
-                transformed_items.append(item)
-
-            # ... and finally render using the right composer. Note
-            # that the message is already queued when we render it.
-            composer = composers[format]
-
-            message = composer.render(sub, transformed_items)
-            queued_messages +=1
-
+            message = render_message(channel, sub, items, use_collector)
+            if message is not None:
+                queued_messages +=1
     return queued_messages
 
 class AbstractPeriodicScheduler(object):
