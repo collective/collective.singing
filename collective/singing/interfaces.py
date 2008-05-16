8a6bd705065db21571897a6ba9a7543119b89bee
nouri <nouri@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-05-16 12:40:41 +0000

    Shuffle around the interfaces to get a meaningful ordering.

--- collective/singing/interfaces.py
+++ collective/singing/interfaces.py
@@ -8,90 +8,38 @@ import z3c.form.interfaces
 
 from collective.singing import MessageFactory as _
 
-class ISalt(interface.Interface):
-    """A utility that's a salt for use in creating secrets.
+class ICollector(interface.Interface):
+    """Collectors are useful for automatic newsletters.  They are
+    responsible for assembling a list of items for publishing.
     """
 
+    title = schema.TextLine(
+        title=u"Title",
+        )
 
-class IRequestBasedSecret(interface.Interface):
-    """A utility that provides a secret based on the request.
-    """
-    def __call__(request):
-        """Return an ASCII secret.
+    optional = schema.Bool(
+        title=u"Subscriber optional",
+        )
+
+    schema = schema.Object(
+        title=u"A schema instance for use in the subscription form",
+        description=u"Values are stored via the ICollectorData adapter per "
+        "subscriber.",
+        required=False,
+        schema=IInterface,
+        )
+
+    def get_items(cue=None, subscription=None):
+        """Return a tuple '(items, cue)' where 'items' is the items
+        that'll be fed into the composer, and 'cue' is an object that
+        can be passed to this method again.
+
+        If 'cue' is passed, I will only return items that are newer
+        than the items that were returned when the cue was retrieved.
+
+        If 'subscription' is given, I will filter according to the
+        subscriber's ICollectorData.
         """
-
-
-class IComposerBasedSecret(interface.Interface):
-    """An adapter for composer that provides a secret based on the
-    user's data used for the composer (see also 'IComposerData').
-    """
-    
-    def secret(data):
-        """Return an ASCII secret.
-
-        'data' is a dict that corresponds to the IComposer's schema.
-        """
-
-
-class ISubscription(IAnnotatable):
-    """A subscription to a channel.
-    """
-
-    channel = schema.Object(
-        title=u"The channel that we're subscribed to",
-        schema=IInterface, # should be really IChannel
-        )
-
-    secret = schema.ASCIILine(
-        title=u"The subscriber's secret, ideally unique across channels",
-        description=u"""\
-        Might be a hash of the subscriber's e-mail and a secret on the
-        server in case we're dealing with an anonymous subscription.
-        Used for unsubscription or for providing an overview of all of
-        a subscriber's subscriptions.
-        """,
-        )
-
-    composer_data = schema.Dict(title=u"Composer data")
-    collector_data = schema.Dict(title=u"Collector data")
-    metadata = schema.Dict(title=u"Metadata")
-
-MESSAGE_STATES = [u'new',   # just added
-                  u'sent',  # sent successfully
-                  u'error', # error while sending
-                  u'retry', # error while sending, but retrying
-                  ]
-
-class IMessage(interface.Interface):
-    """Messages are objects ready for sending.
-    """
-    payload = schema.Field(
-        title=u"The message's payload, e.g. the e-mail message.",
-        )
-
-    subscription = schema.Object(
-        title=u"Subscription, referenced for bookkeeping purposes only.",
-        schema=ISubscription,
-        )
-
-    status = schema.Choice(
-        title=u"State",
-        description=u"IMessageChanged is fired automatically when this is set",
-        values=MESSAGE_STATES)
-
-    status_message = schema.Text(
-        title=u"Status details",
-        required=False)
-
-    status_changed = schema.Datetime(
-        title=u"Last time this message changed its status",
-        )
-
-class IMessageChanged(zope.lifecycleevent.interfaces.IObjectModifiedEvent):
-    """An object event on the message that signals that the status has
-    changed.
-    """
-    old_status = schema.TextLine(title=u"Old status of message")
 
 
 class IComposer(interface.Interface):
@@ -131,60 +79,6 @@ class IComposer(interface.Interface):
         the user to his subscription page.
         """
 
-class ISubscriptionLabel(interface.Interface):
-    """Marker interface for field on IComposerSchema that labels a
-    subscription.
-    """
-
-class ISubscriptionKey(interface.Interface):
-    """Marker interface for field on IComposerSchema that serves as a
-    key for conflicts.
-
-    For subscriptions per e-mail this is the e-mail field.  No
-    subscriptions for the same format and same e-mail address will be
-    allowed, then.
-    """
-
-class ICollector(interface.Interface):
-    """Collectors are useful for automatic newsletters.  They are
-    responsible for assembling a list of items for publishing.
-    """
-
-    title = schema.TextLine(
-        title=u"Title",
-        )
-
-    optional = schema.Bool(
-        title=u"Subscriber optional",
-        )
-
-    schema = schema.Object(
-        title=u"A schema instance for use in the subscription form",
-        description=u"Values are stored via the ICollectorData adapter per "
-        "subscriber.",
-        required=False,
-        schema=IInterface,
-        )
-
-    def get_items(cue=None, subscription=None):
-        """Return a tuple '(items, cue)' where 'items' is the items
-        that'll be fed into the composer, and 'cue' is an object that
-        can be passed to this method again.
-
-        If 'cue' is passed, I will only return items that are newer
-        than the items that were returned when the cue was retrieved.
-
-        If 'subscription' is given, I will filter according to the
-        subscriber's ICollectorData.
-        """
-
-class ITemplate(interface.Interface):
-    """Templates are useful for generating and presenting newsletters.
-    """
-
-    title = schema.TextLine(
-        title=u"Title",
-        )
 
 class IScheduler(interface.Interface):
     """A scheduler triggers the sending of messages periodically.
@@ -215,6 +109,7 @@ class IScheduler(interface.Interface):
         A manual override.
         """
 
+
 class ISubscriptions(interface.Interface):
     """The `subscriptions` attribute of IChannel implements this.
 
@@ -240,6 +135,47 @@ class ISubscriptions(interface.Interface):
 
         Available fields are: 'fulltext', 'secret', 'format', 'key', 'label'.
         """
+
+
+class ISubscription(IAnnotatable):
+    """A subscription to a channel.
+    """
+
+    channel = schema.Object(
+        title=u"The channel that we're subscribed to",
+        schema=IInterface, # should be really IChannel
+        )
+
+    secret = schema.ASCIILine(
+        title=u"The subscriber's secret, ideally unique across channels",
+        description=u"""\
+        Might be a hash of the subscriber's e-mail and a secret on the
+        server in case we're dealing with an anonymous subscription.
+        Used for unsubscription or for providing an overview of all of
+        a subscriber's subscriptions.
+        """,
+        )
+
+    composer_data = schema.Dict(title=u"Composer data")
+    collector_data = schema.Dict(title=u"Collector data")
+    metadata = schema.Dict(title=u"Metadata")
+
+
+class ISubscriptionLabel(interface.Interface):
+    """Marker interface for field on IComposerSchema that labels a
+    subscription.
+    """
+
+
+class ISubscriptionKey(interface.Interface):
+    """Marker interface for field on IComposerSchema that serves as a
+    key for conflicts.
+
+    For subscriptions per e-mail this is the e-mail field.  No
+    subscriptions for the same format and same e-mail address will be
+    allowed, then.
+    """
+
 
 class IMessageQueues(IMapping):
     """A dict that contains one ``zc.queue.interfaces.IQueue`` per
@@ -299,24 +235,78 @@ class IChannel(interface.Interface):
         )
 
 
-class IFormatItem(interface.Interface):
-    """A view that formats an item for use in a newsletter.
-
-    Given an item as retrieved from a collector, this view returns a
-    representation of the given item ready for inclusion in the
-    message via the ``IComposer.render`` method.
+class IChannelLookup(interface.Interface):
+    """A utility that looks up all channels in a site.
     """
 
     def __call__():
-        """Returns a unicode-string."""
-
-class ITransform(interface.Interface):
-    """An adapter that allows to rewrite links and the like in
-    outgoing messages.
-    """
-    def __call__(text, subscription):
-        """Return transformed text
+        """Return a list of ``IChannel`` objects.
         """
+
+
+class ISalt(interface.Interface):
+    """A utility that's a salt for use in creating secrets.
+    """
+
+
+class IRequestBasedSecret(interface.Interface):
+    """A utility that provides a secret based on the request.
+    """
+    def __call__(request):
+        """Return an ASCII secret.
+        """
+
+
+class IComposerBasedSecret(interface.Interface):
+    """An adapter for composer that provides a secret based on the
+    user's data used for the composer (see also 'IComposerData').
+    """
+    
+    def secret(data):
+        """Return an ASCII secret.
+
+        'data' is a dict that corresponds to the IComposer's schema.
+        """
+
+
+MESSAGE_STATES = [u'new',   # just added
+                  u'sent',  # sent successfully
+                  u'error', # error while sending
+                  u'retry', # error while sending, but retrying
+                  ]
+
+class IMessage(interface.Interface):
+    """Messages are objects ready for sending.
+    """
+    payload = schema.Field(
+        title=u"The message's payload, e.g. the e-mail message.",
+        )
+
+    subscription = schema.Object(
+        title=u"Subscription, referenced for bookkeeping purposes only.",
+        schema=ISubscription,
+        )
+
+    status = schema.Choice(
+        title=u"State",
+        description=u"IMessageChanged is fired automatically when this is set",
+        values=MESSAGE_STATES)
+
+    status_message = schema.Text(
+        title=u"Status details",
+        required=False)
+
+    status_changed = schema.Datetime(
+        title=u"Last time this message changed its status",
+        )
+
+
+class IMessageChanged(zope.lifecycleevent.interfaces.IObjectModifiedEvent):
+    """An object event on the message that signals that the status has
+    changed.
+    """
+    old_status = schema.TextLine(title=u"Old status of message")
+
 
 class IDispatch(interface.Interface):
     """Dispatchers adapt message *payloads* and send them."""
@@ -332,21 +322,37 @@ class IDispatch(interface.Interface):
         If this method raises an exception, an 'error' is assumed.
         """
 
-class IChannelLookup(interface.Interface):
-    """A utility that looks up all channels in a site.
+class IFormatItem(interface.Interface):
+    """A view that formats an item for use in a newsletter.
+
+    Given an item as retrieved from a collector, this view returns a
+    representation of the given item ready for inclusion in the
+    message via the ``IComposer.render`` method.
     """
 
     def __call__():
-        """Return a list of ``IChannel`` objects.
+        """Returns a unicode-string."""
+
+
+class ITransform(interface.Interface):
+    """An adapter that allows to rewrite links and the like in
+    outgoing messages.
+    """
+    def __call__(text, subscription):
+        """Return transformed text
         """
+
 
 class IFormLayer(z3c.form.interfaces.IFormLayer):
     pass
 
+
 class IDynamicVocabularyCollection(zope.schema.interfaces.ICollection):
-    """ Used to mark ICollection Fields in our dynamic schemas.
-        To indicate that thay have dynamic vocabularies.
-        We need a more tolerant z3c.form.interfaces.IDataConverter
-        in this case - one that drops saved values that are no longer
-        in the vocabulary when updating widgets.
-        """
+    """Used to mark ICollection Fields in our dynamic schemas.  To
+    indicate that thay have dynamic vocabularies.  We need a more
+    tolerant z3c.form.interfaces.IDataConverter in this case - one
+    that drops saved values that are no longer in the vocabulary when
+    updating widgets.
+
+    (XXX: Mabye this should go into the browser package?)
+    """
