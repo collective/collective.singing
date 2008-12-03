3807f522b49b1dee4de45c4357fd8083191fee26
saily <saily@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-12-03 19:33:32 +0000

    preparing release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.6.7'
+version = '0.6.8'
 
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
