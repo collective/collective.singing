68ca724682997c042f8cc9b05055c8e6cd5a7532
nouri <nouri@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-02-01 17:28:24 +0000

    Removed version pinning of zope.app.catalog to <= 3.6.0.  This is handled through our external extends file now.  See https://svn.plone.org/svn/collective/collective.dancing/buildout-extends/0.8.7.cfg and http://pypi.python.org/pypi/collective.dancing#installation

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='collective.singing',
       install_requires=[
           'setuptools',
           'zope.sendmail',
-          'zope.app.catalog<3.6.0',
+          'zope.app.catalog',
           'zc.queue',
           'zc.lockfile',
           'plone.z3cform>=0.5.1',
