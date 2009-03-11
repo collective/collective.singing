b968e5a448dded41a0b0bcfc078003ac11ee4779
nouri <nouri@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-03-11 15:54:57 +0000

    Prepare for 0.6.11 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.6.11'
+version = '0.6.12'
 
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
