9db6de18642024a5f0837cd331b12893cde708ac
regebro <regebro@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-04-07 20:28:26 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.6.14'
+version = '0.6.15'
 
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
