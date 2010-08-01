4f2758fcd44f83ce99acdae9f43988ad6f4b2e82
regebro <regebro@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-08-01 07:11:13 +0000

    Back to development: 0.6.16

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.6.15'
+version = '0.6.16dev'
 
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
