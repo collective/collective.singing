b6dc8e24871fd0773aa880105348c3a2ec716eb2
saily <saily@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-11-04 14:11:47 +0000

    Prepare collective.singing 0.7.0.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.6.16'
+version = '0.7.0'
 
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
